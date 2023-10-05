c90a324f28bb25bb7369a0c3525637a44283fee3
Andi <alexandra.e.burke@gmail.com>
2023-10-05 08:54:07 -0400

    UploadedMathApp

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+public class MathApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declaring Gary, Bob, and highest salary
+        double garySalary = 67000.00;
+        double bobSalary = 52000.00;
+        double highestSalary = Math.max(garySalary, bobSalary);
+        System.out.println("The highest salary is " + highestSalary);
+        // Question 2
+        //declaring Car Prices
+        double carPrice = 75000.99;
+        double truckPrice = 134000.99;
+        double smallestPrice = Math.min(carPrice, truckPrice);
+        System.out.println("The smallest price is " + smallestPrice);
+        //Question 3
+        //Find Radius of a circle
+
+    }
+
+}
