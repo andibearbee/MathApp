bbb9f0a4e9a1ccfac886c6fdfd4c14963604d47f
Andi <alexandra.e.burke@gmail.com>
2023-10-05 10:09:44 -0400

    MathAppUpdateQ345

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -14,8 +14,41 @@ public class MathApp {
         double truckPrice = 134000.99;
         double smallestPrice = Math.min(carPrice, truckPrice);
         System.out.println("The smallest price is " + smallestPrice);
-        //Question 3
-        //Find Radius of a circle
+        // Question 3
+        //  Find and display the area of a circle whose radius is 7.25
+
+
+        double radius = 7.25;
+        double area = Math.PI * Math.pow(radius , 2);
+        System.out.println("The area of a circle with a radius of " + radius + " is " + area);
+
+
+
+        //Question 4
+        // Find and display the square root a variable after it is set to 5.0
+
+        double number = 5.0;
+        double sqrtValue = Math.sqrt(5.0);
+
+        System.out.println("sqrt of " + number + " is " + sqrtValue);
+
+
+
+//        Question 5
+//        Find and display the distance between the points (5, 10) and (85, 50)
+
+        int x1 = 5;
+        int y1 = 10;
+        int x2 = 85;
+        int y2 = 50;
+        double distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        System.out.println("The distance between some points "+ distance);
+
+
+
+
+
 
     }
 
